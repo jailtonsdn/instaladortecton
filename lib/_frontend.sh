469bc3e44fdf21ab3c46667f29d6a46ec9204484
Jailton Nascimento <108580162+jailtonsdn@users.noreply.github.com>
2024-04-30 13:23:42 -0300

    Update _frontend.sh

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -1,7 +1,62 @@
+#!/bin/bash
+# 
+# Funções para configurar o frontend do aplicativo
+
 #######################################
-# Sets frontend environment variables
-# Arguments:
-#   None
+# Instala pacotes do Node.js
+# Argumentos:
+#   Nenhum
+#######################################
+frontend_node_dependencies() {
+  print_banner
+  printf "${WHITE} 💻 Instalando dependências do frontend...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo -u deploy npm install --prefix /home/deploy/${instancia_add}/frontend
+}
+
+#######################################
+# Compila o código do frontend
+# Argumentos:
+#   Nenhum
+#######################################
+frontend_node_build() {
+  print_banner
+  printf "${WHITE} 💻 Compilando o código do frontend...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo -u deploy npm run build --prefix /home/deploy/${instancia_add}/frontend
+}
+
+#######################################
+# Atualiza o código do frontend
+# Argumentos:
+#   Nenhum
+#######################################
+frontend_update() {
+  print_banner
+  printf "${WHITE} 💻 Atualizando o frontend...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo -u deploy pm2 stop ${instancia_atualizar}-frontend
+  sudo -u deploy git -C /home/deploy/${instancia_atualizar} pull
+  sudo -u deploy npm install --prefix /home/deploy/${instancia_atualizar}/frontend
+  sudo -u deploy rm -rf /home/deploy/${instancia_atualizar}/frontend/build
+  sudo -u deploy npm run build --prefix /home/deploy/${instancia_atualizar}/frontend
+  sudo -u deploy pm2 start /home/deploy/${instancia_atualizar}/frontend/server.js --name ${instancia_atualizar}-frontend
+  sudo -u deploy pm2 save
+}
+
+#######################################
+# Configura variáveis de ambiente para o frontend
+# Argumentos:
+#   Nenhum
 #######################################
 frontend_set_env() {
   print_banner
@@ -10,22 +65,13 @@ frontend_set_env() {
 
   sleep 2
 
-  # Adjust backend URL to use HTTP
-  backend_url="http://${backend_url}:${backend_porta}"
-
-sudo su - deploy << EOF
-  cat <<[-]EOF > /home/deploy/${instancia_add}/frontend/.env
-REACT_APP_BACKEND_URL=${backend_url};${backend_porta}
+  sudo -u deploy cat <<EOF > /home/deploy/${instancia_add}/frontend/.env
+REACT_APP_BACKEND_URL=http://localhost:${backend_port}
 REACT_APP_HOURS_CLOSE_TICKETS_AUTO=24
-BACKEND_PROTOCOL=http
-[-]EOF
 EOF
 
-  sleep 2
-
-sudo su - deploy << EOF
-  cat <<[-]EOF > /home/deploy/${instancia_add}/frontend/server.js
-//simple express server to run frontend production build;
+  sudo -u deploy cat <<EOF > /home/deploy/${instancia_add}/frontend/server.js
+// Simple express server to run frontend production build;
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -34,9 +80,56 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(${frontend_port});
-
-[-]EOF
 EOF
+}
+
+#######################################
+# Inicia o frontend usando pm2
+# Argumentos:
+#   Nenhum
+#######################################
+frontend_start_pm2() {
+  print_banner
+  printf "${WHITE} 💻 Iniciando pm2 (frontend)...${GRAY_LIGHT}"
+  printf "\n\n"
 
   sleep 2
+
+  sudo -u deploy pm2 start /home/deploy/${instancia_add}/frontend/server.js --name ${instancia_add}-frontend
+  sudo -u deploy pm2 save
+}
+
+#######################################
+# Configura o nginx para o frontend
+# Argumentos:
+#   Nenhum
+#######################################
+frontend_nginx_setup() {
+  print_banner
+  printf "${WHITE} 💻 Configurando nginx (frontend)...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo cat <<EOF > /etc/nginx/sites-available/${instancia_add}-frontend
+server {
+  listen 80;
+  server_name ${frontend_url};
+
+  location / {
+    proxy_pass http://localhost:${frontend_port};
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
+  }
+}
+EOF
+
+  sudo ln -s /etc/nginx/sites-available/${instancia_add}-frontend /etc/nginx/sites-enabled/
+  sudo systemctl restart nginx
 }
