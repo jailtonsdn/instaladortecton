6c5686a5b8666cc77e491a3aa637b8293e66cbe8
Jailton Nascimento <108580162+jailtonsdn@users.noreply.github.com>
2024-07-25 09:42:56 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -29,7 +29,7 @@ EOF
 #######################################
 system_git_clone() {
   print_banner
-  printf "${WHITE} 💻 Fazendo download do código Equipechat...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Fazendo download do código TonChat...${GRAY_LIGHT}"
   printf "\n\n"
 
 
@@ -49,7 +49,7 @@ EOF
 #######################################
 system_update() {
   print_banner
-  printf "${WHITE} 💻 Vamos atualizar o sistema Equipechat...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos atualizar o sistema TonChat...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -71,7 +71,7 @@ EOF
 #######################################
 deletar_tudo() {
   print_banner
-  printf "${WHITE} 💻 Vamos deletar o Equipechat...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos deletar o TonChat...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -117,7 +117,7 @@ EOF
 #######################################
 configurar_bloqueio() {
   print_banner
-  printf "${WHITE} 💻 Vamos bloquear o Equipechat...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos bloquear o TonChat...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -144,7 +144,7 @@ EOF
 #######################################
 configurar_desbloqueio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Desbloquear o Equipechat...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos Desbloquear o TonChat...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -170,7 +170,7 @@ EOF
 #######################################
 configurar_dominio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Alterar os Dominios do Equipechat...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos Alterar os Dominios do TonChat...${GRAY_LIGHT}"
   printf "\n\n"
 
 sleep 2
